94453d2be8766df401202b8110662d50a68b808b
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-05-25 17:16:07 +0530

    filter: Get characters with mass greater than 100

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const characters = [
+  {
+    name: "Luke Skywalker",
+    height: "172",
+    mass: "77",
+    eye_color: "blue",
+    gender: "male",
+  },
+  {
+    name: "Darth Vader",
+    height: "202",
+    mass: "136",
+    eye_color: "yellow",
+    gender: "male",
+  },
+  {
+    name: "Leia Organa",
+    height: "150",
+    mass: "49",
+    eye_color: "brown",
+    gender: "female",
+  },
+  {
+    name: "Anakin Skywalker",
+    height: "188",
+    mass: "84",
+    eye_color: "blue",
+    gender: "male",
+  },
+];
+
+// FILTER
+const gtcharacters100 = characters.filter(
+    (character) => character.mass > 100
+    );
+console.log(gtcharacters100);
