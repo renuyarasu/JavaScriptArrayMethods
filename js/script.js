2686a7dc509807d66e44c26b3db1b1c4427b61dc
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-01 16:58:51 +0530

    Array Equality: Method 04 - using _.isEqual() form Lodash

--- js/script.js
+++ js/script.js
@@ -34,16 +34,6 @@ console.clear();
 const oldTags = ['Facebook', 'Twitter', 'LinkedIn', 'YouTube', 'Pinterest', 'Instagram', 'Tumblr', 'Flickr'];
 const newTags = ['YouTube', 'Pinterest', 'Instagram', 'Tumblr', 'Flickr', 'Facebook', 'Twitter', 'LinkedIn'];
 
-//Method 03: using .includes()
+//Method 04: using _.isEqual() form Lodash
 
-let array_01_status = oldTags.reduce(
-  (acc, value) =>  acc && newTags.includes(value), 
-  true
-);
-
-let array_02_status = newTags.reduce(
-  (acc, value) => acc && oldTags.includes(value), 
-  true
-);
-
-console.log('arrayEquals: ', array_01_status && array_02_status); //true
+console.log('arrayEquals: ', _.isEqual(oldTags.sort(), newTags.sort())); //true
